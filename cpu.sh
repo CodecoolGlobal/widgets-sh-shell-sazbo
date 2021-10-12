028961739bcb3dea61fb11d8e160fee2facde084
Stark Máté <strkmt@gmail.com>
2021-10-12 15:35:57 +0200

    feat: (#2)(#3)(#4) implement CPU Stats, Process Stats and Memory Stats

---
+++ cpu.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo $(grep -m 1 'model name' /proc/cpuinfo)
+echo $(grep -m 1 'cpu cores' /proc/cpuinfo)
+echo $(grep -m 1 'cpu MHz' /proc/cpuinfo)
+#cat /proc/cpuinfo
+
--- mem.sh
+++ mem.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+echo $(free | awk '/Mem/{print("RAM Usage: "),$3, "of", $2 $3/$2*100}')
