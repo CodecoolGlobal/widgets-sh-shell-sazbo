fbf7b8895a8f74f44adfc0b996972af0e86f9ef6
Stark Máté <strkmt@gmail.com>
2021-10-12 16:58:45 +0200

    feat: (#6) implement Users and Groups

--- user.sh
+++ user.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+FILENAME="/etc/passwd"
+count=0
+while IFS=: read -r username password userid groupid comment homedir cmdshell
+do
+  #echo "$username, $userid, $comment $homedir"
+  if [ "$userid" -ge 1000 ]
+  then
+      ((count++))
+  fi
+done < $FILENAME
+
+echo "Number of non-system users: $count"
+echo "Number of groups: $(groups | wc -w)"
+
